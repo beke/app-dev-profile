0a73f39fd032c3622eb5c55d1b3fef4497fcdf95
Bilgutei Erdenebayar <beke0130@gmail.com>
2020-02-21 15:42:05 +0800

    Initial commit

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Bilgutei on 2/21/20.
+//  Copyright © 2020 Bilgutei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
